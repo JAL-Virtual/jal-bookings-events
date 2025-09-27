4e3523889f1141fc55472cf6fd4716fa53f9d0cc
BunnyCoding <z.yong@poomjit.com>
2025-09-27 22:56:18 +0800

    Refactor SlotPageHeader and UserSlotsSideInfos components to improve code structure and enhance UI consistency. Update import paths for better organization, streamline prop handling, and enhance styling for buttons and typography. Ensure better accessibility and maintainability across both components.

--- src/components/UserSlotsSideInfos.tsx
+++ src/components/UserSlotsSideInfos.tsx
@@ -1,40 +1,44 @@
 import { FunctionComponent } from "react";
 import { VerticalInfoCard } from "./InfoCard";
-import { Header, Subheader } from "./Typography";
+import { Header, Subheader } from "./typography/Typography";
 import { FiDownload, FiMap } from "react-icons/fi";
-import { LinkButton } from "./Button";
-import { useText } from "../hooks/useText";
+import { LinkButton } from "./buttons/LinkButton";
+import { useText } from "../../hooks/useText";
 
 interface UserSlotsSideInfosProps {
-    pilotBriefing: string;
+  pilotBriefing: string;
 }
 
-export const UserSlotsSideInfos: FunctionComponent<UserSlotsSideInfosProps> = ({ pilotBriefing }) => {
-    const { t } = useText();
-    
-    return (
-        <aside className="px-6 pt-9 bg-white dark:bg-black h-full">
-            <Header textSize="text-lg" textColor="text-blue dark:text-white">{ t('myFlights.title') }</Header>
-            <Subheader textSize="text-md" textColor="text-light-blue dark:text-white">{ t('myFlights.subtitle') }</Subheader>
-            <div className="mt-12">
-                <VerticalInfoCard
-                    icon={<FiMap size={25} />}
-                    header={ t('info.pilotBriefing.title') }
-                    content={ t('info.pilotBriefing.description') }
-                >
-                    <LinkButton
-                        width="w-full"
-                        height="h-9"
-                        icon={<FiDownload size={18} />}
-                        href={pilotBriefing}
-                        content={
-                            <span className="block w-full px-8 text-center font-semibold text-xs text-white">
-                                { t('generics.see') }
-                            </span>
-                        }
-                    />
-                </VerticalInfoCard>
-            </div>
-        </aside>
-    )
+export function UserSlotsSideInfos({ pilotBriefing }: UserSlotsSideInfosProps) {
+  const { t } = useText();
+  
+  return (
+    <aside className="px-6 pt-9 bg-white dark:bg-black h-full">
+      <Header textSize="text-lg" textColor="text-blue-600 dark:text-white">
+        {t('myFlights.title')}
+      </Header>
+      <Subheader textSize="text-md" textColor="text-blue-500 dark:text-white">
+        {t('myFlights.subtitle')}
+      </Subheader>
+      <div className="mt-12">
+        <VerticalInfoCard
+          icon={<FiMap size={25} />}
+          header={t('info.pilotBriefing.title')}
+          content={t('info.pilotBriefing.description')}
+        >
+          <LinkButton
+            width="w-full"
+            height="h-9"
+            icon={<FiDownload size={18} />}
+            href={pilotBriefing}
+            content={
+              <span className="block w-full px-8 text-center font-semibold text-xs text-white">
+                {t('generics.see')}
+              </span>
+            }
+          />
+        </VerticalInfoCard>
+      </div>
+    </aside>
+  );
 }
