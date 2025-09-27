0c19fe8ad907b7c914b84ff97fd20e73e5878bd0
BunnyCoding <z.yong@poomjit.com>
2025-09-27 23:00:19 +0800

    Refactor useEventList hook to improve query structure and organization. Update import paths for better clarity and consistency, transitioning to the new '@tanstack/react-query' package. Enhance query configuration for improved maintainability and performance.

--- src/hooks/event/useEventList.ts
+++ src/hooks/event/useEventList.ts
@@ -1,17 +1,19 @@
 import { useContext } from 'react';
 import { AxiosError } from 'axios';
-import { useInfiniteQuery } from 'react-query';
-import { ONE_DAY } from "appConstants";
-import { IocContext } from 'context/IocContext';
-import { Event } from "types/Event";
-import { Pagination } from 'types/Pagination';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { ONE_DAY } from "../../constants/appConstants";
+import { IocContext } from '../../contexts/IocContext';
+import { Event } from "../../types/Event";
+import { Pagination } from '../../types/ApiBase';
 
 export const useEvents = (page = 1, perPage = 6) => {
 	const { apiClient } = useContext(IocContext);
 
-	const eventList = useInfiniteQuery<Pagination<Event>, AxiosError>('events', async ({ pageParam = page }) => {
-		return await apiClient.getEvents({ page: pageParam, perPage });
-	}, {
+	const eventList = useInfiniteQuery<Pagination<Event>, AxiosError>({
+		queryKey: ['events'],
+		queryFn: async ({ pageParam = page }) => {
+			return await apiClient.getEvents({ page: pageParam, perPage });
+		},
 		staleTime: ONE_DAY,
 		getNextPageParam: (lastPage, _) => {
 			return (lastPage.page * lastPage.perPage) >= lastPage.total ? undefined : lastPage.page + 1;
